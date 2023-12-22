cdc251700032921d6a0838a84c8af75e03cbde79
SwimmyE <68501208+SwimmyE@users.noreply.github.com>
2023-12-22 10:36:04 +0530

    Delete src/main/java/com/company/todoapp/configurations/SwaggerConfig.java

--- src/main/java/com/company/todoapp/configurations/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.company.todoapp.configurations;
-
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-@Configuration
-@EnableSwagger2WebMvc
-public class SwaggerConfig {
-}
